13449d59733b7cbaf5edeb7ef5b57f0ecb053320
Bima42 <tanpau42@gmail.com>
2026-01-20 09:26:53 +0100

    fix: toggle theme button

--- app/src/components/ui/theme-toggle.tsx
+++ app/src/components/ui/theme-toggle.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 import { SimpleButton } from '@/components/ui/simple-button';
+import { Button } from '@/components/ui/button';
 
 export function ThemeToggle() {
     const { theme, setTheme } = useTheme();
@@ -18,12 +19,8 @@ export function ThemeToggle() {
     }
 
     return (
-        <SimpleButton
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            icon={theme === 'dark' ? Moon : Sun}
-            size="lg"
-            label="Toggle theme"
-            noHover={false}
-        />
+        <Button variant="ghost" size="sm" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+            {theme === 'dark' ? <Moon className="h-5 w-5" /> : <Sun className="h-5 w-5" />}
+        </Button>
     );
 }
